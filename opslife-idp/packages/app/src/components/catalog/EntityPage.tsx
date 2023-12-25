f4b4978bac6a6340a8e1d4b53512a8879493bbd4
Dionizio Ferreira <dionizioaf@gmail.com>
2023-12-25 10:46:46 -0300

    testando

--- opslife-idp/packages/app/src/components/catalog/EntityPage.tsx
+++ opslife-idp/packages/app/src/components/catalog/EntityPage.tsx
@@ -58,6 +58,12 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 
+import {
+  EntityCircleCIContent,
+  isCircleCIAvailable,
+} from '@circleci/backstage-plugin';
+
+
 const techdocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
@@ -89,6 +95,10 @@ const cicdContent = (
           </Button>
         }
       />
+    </EntitySwitch.Case>
+    
+    <EntitySwitch.Case if={isCircleCIAvailable}>
+      <EntityCircleCIContent />
     </EntitySwitch.Case>
   </EntitySwitch>
 );
